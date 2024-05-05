ca262752052dd063caa2975b0caa343e41e5c91b
xiao6 <zexiliu@outlook.com>
2024-05-05 22:11:50 +1000

    bypass cors in development

--- takeHomeToDoBackend/Program.cs
+++ takeHomeToDoBackend/Program.cs
@@ -14,6 +14,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors(options =>
+    {
+        options.WithOrigins("http://localhost:4200")
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
 }
 
 app.UseHttpsRedirection();
