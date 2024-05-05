7dc0a412892f6a8f605c0f3d4bca9d3c67340adc
xiao6 <zexiliu@outlook.com>
2024-05-05 21:50:43 +1000

    get function, style, html, calling server

--- takeHomeToDoClient/todo-app/src/app/app.component.ts
+++ takeHomeToDoClient/todo-app/src/app/app.component.ts
@@ -1,13 +1,70 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
+interface ToDo {
+  id: number;
+  task: string;
+  priority: number;
+  description: string;
+  isComplete: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  providers: [
+    { provide: 'BASE_API_URL', useValue: 'https://localhost:7243' } // Change this to your server's base URL
+  ],
+  standalone: true,
+  imports: [
+    FormsModule,
+    CommonModule,
+    HttpClientModule,
+  ],
 })
-export class AppComponent {
-  title = 'todo-app';
+export class AppComponent implements OnInit {
+  public forecasts: WeatherForecast[] = [];
+  public toDos: ToDo[] = [];
+
+  constructor(private http: HttpClient, @Inject('BASE_API_URL') private baseUrl: string) { }
+
+  ngOnInit() {
+    this.getTodos();
+  }
+
+  getTodos() {
+    this.http.get<ToDo[]>(`${this.baseUrl}/ToDos`).subscribe(
+      (result) => {
+        this.toDos = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  getForecasts() {
+    this.http.get<WeatherForecast[]>(`${this.baseUrl}/weatherforecast`).subscribe(
+      (result) => {
+        this.forecasts = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  toggleCompletion(item: ToDo) {
+    item.isComplete = !item.isComplete;
+  }
 }
