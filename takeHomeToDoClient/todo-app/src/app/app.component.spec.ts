11cf513befce1d5189002b98cd04c9486287eeb0
xiao6 <zexiliu@outlook.com>
2024-05-06 00:28:00 +1000

    remove redundant test code

--- takeHomeToDoClient/todo-app/src/app/app.component.spec.ts
+++ takeHomeToDoClient/todo-app/src/app/app.component.spec.ts
@@ -14,11 +14,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'todo-app' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('todo-app');
-  });
+  // it(`should have the 'todo-app' title`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('todo-app');
+  // });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
